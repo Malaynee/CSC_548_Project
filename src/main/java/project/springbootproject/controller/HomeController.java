5bbe5efe2486557d158bc126e8befe1645f9daa5
Mal <142328422+Malaynee@users.noreply.github.com>
2025-11-21 14:23:47 -0600

    Updated HomeController.java
    updated to pass session data

--- src/main/java/project/springbootproject/controller/HomeController.java
+++ src/main/java/project/springbootproject/controller/HomeController.java
@@ -1,7 +1,9 @@
 package project.springbootproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.servlet.http.HttpSession;
 
 /**
  * Controller for the home page
@@ -12,10 +14,18 @@ public class HomeController {
     
     /**
      * Handles GET requests to the root URL "/"
-     * Returns the home page (index.html)
+     * Returns the home page (index.html from templates folder)
+     * Passes session data to the view for personalization
      */
     @GetMapping("/")
-    public String home() {
+    public String home(HttpSession session, Model model) {
+        // Get username from session if logged in
+        String username = (String) session.getAttribute("username");
+        
+        if (username != null) {
+            model.addAttribute("username", username);
+        }
+        
         return "index";
     }
 }
